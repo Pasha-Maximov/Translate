5dcd98b6bd1033f7afbe2faeb9f0972881cbd9db
Pasha-Maximov <167475063+Pasha-Maximov@users.noreply.github.com>
2024-07-14 14:20:08 -0400

    k

---
+++ h.py
@@ -0,0 +1,8 @@
+X=0
+Y=5
+if X==0:
+    print("no. I refuse to divide by 0.")
+    exit
+elif:
+    Z=(Y/X)
+    print (Y,"/",X,"=",Z)
--- turtle group hw.py
+++ turtle group hw.py
@@ -25,104 +25,146 @@ def draw_ellipse(center_x, center_y, radius_x, radius_y, angle, extent=360):
 
 
 
-paper.penup()
-paper.back(15)
-paper.color("green")
-paper.pendown()
-paper.begin_fill()
-paper.forward(30)
-paper.right(90)
-paper.forward(300)
-paper.right(90)
-paper.forward(30)
-paper.right(90)
-paper.forward(300)
-paper.right(90)
-paper.end_fill()
-
-paper.color("white")
-
-paper.begin_fill()
-draw_ellipse(50,0,100,20,180,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(-50,0,100,20,0,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(0,50,100,25,90,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(0,-50,100,25,270,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(25,25,100,20,45,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(25,-25,100,20,135,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(-25,-25,100,20,225,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(-25,25,100,20,315,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(12.5,12.5,100,20,22.5,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(12.5,-12.5,100,20,112.5,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(-12.5,-12.5,100,20,202.5,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(-12.5,12.5,100,20,292.5,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(6.25,6.25,100,20,72.5,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(6.25,-6.25,100,20,162.5,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(-6.25,-6.25,100,20,252.5,360)
-paper.end_fill()
-paper.begin_fill()
-draw_ellipse(-6.25,6.25,100,20,342.5,360)
-paper.end_fill()
-
-paper.penup()
-paper.home()
-paper.back(50)
-paper.pendown()
-paper.right(90)
-paper.color("white","yellow")
-paper.begin_fill()
-paper.circle(50)
-paper.end_fill()
-paper.color("white")
-paper.left(90)
-
-paper.penup()
-paper.home()
-paper.back(50)
-paper.pendown()
-paper.right(90)
-paper.color("black")
-paper.pensize(1)
-paper.circle(50)
-paper.pensize(2)
-paper.color("white")
-paper.left(90)
+input= input
+print ("What do you want drawn?")
+print ("1) Daisy")
+print ("2) _____")
+print ("3) _____")
+print ("4) _____")
+print ("if you would not like an object to be drawn or do not want to continue/want to exit, please input no or exit.")
+print (input)
 
 
 
 
+if input ("∆" or "1" or "daisy"):
+    paper.penup()
+    paper.back(15)
+    paper.color("green")
+    paper.pendown()
+    paper.begin_fill()
+    paper.forward(30)
+    paper.right(90)
+    paper.forward(300)
+    paper.right(90)
+    paper.forward(30)
+    paper.right(90)
+    paper.forward(300)
+    paper.right(90)
+    paper.end_fill()
+
+    paper.color("white")
+
+    paper.begin_fill()
+    draw_ellipse(50,0,100,20,180,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(-50,0,100,20,0,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(0,50,100,25,90,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(0,-50,100,25,270,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(25,25,100,20,45,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(25,-25,100,20,135,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(-25,-25,100,20,225,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(-25,25,100,20,315,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(12.5,12.5,100,20,22.5,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(12.5,-12.5,100,20,112.5,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(-12.5,-12.5,100,20,202.5,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(-12.5,12.5,100,20,292.5,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(6.25,6.25,100,20,72.5,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(6.25,-6.25,100,20,162.5,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(-6.25,-6.25,100,20,252.5,360)
+    paper.end_fill()
+    paper.begin_fill()
+    draw_ellipse(-6.25,6.25,100,20,342.5,360)
+    paper.end_fill()
+
+    paper.penup()
+    paper.home()
+    paper.back(50)
+    paper.pendown()
+    paper.right(90)
+    paper.color("white","yellow")
+    paper.begin_fill()
+    paper.circle(50)
+    paper.end_fill()
+    paper.color("white")
+    paper.left(90)
+
+    paper.penup()
+    paper.home()
+    paper.back(50)
+    paper.pendown()
+    paper.right(90)
+    paper.color("black")
+    paper.pensize(1)
+    paper.circle(50)
+    paper.pensize(2)
+    paper.color("white")
+    paper.left(90)
+
+    paper.hideturtle()
+
+    turtle.done()
+
+
+    print ("do you want another one to be drawn? If so, select another option.")
+    print ("1) Daisy")
+    print ("2) _____")
+    print ("3) _____")
+    print ("4) _____")
+    print ("if you would not like an object to be drawn or do not want to continue/want to exit, please input no or exit.")
+    print (input)
 
 
 
 
 
-paper.hideturtle()
 
-turtle.done()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if input != ("∆" or "1" or "daisy"):
+    print ("please select one of the given options.")
+    print (input)
+
+
+if input ("■" or "no" or "exit"):
+    exit
